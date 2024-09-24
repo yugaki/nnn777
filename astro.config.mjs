dbf1d01519940eb6cf95c12590165847038c6ead
y_yoshida <y_yoshida@fieldweb.co.jp>
2024-09-24 11:30:48 +0900

    errorrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import lottie from 'astro-integration-lottie';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-   image: {
-    domains: ["nnn777.assets.newt.so","storage.googleapis.com"]
+  integrations: [tailwind(), lottie()], // tailwind と lottie を同時に使う
+  image: {
+    domains: ["nnn777.assets.newt.so", "storage.googleapis.com"], // 画像のドメインを許可
   },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
+/// <reference types="astro-integration-lottie/env" />
 interface ImportMetaEnv {
   readonly NEWT_SPACE_UID: string
   readonly NEWT_CDN_API_TOKEN: string
